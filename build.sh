d8f645207a04ce5e1c5518547070cc5466b2f15f
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 23:03:06 +0100

    Add label, track image history

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
+set -x
+curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
+source src/tmp/install/functions.sh
 
+patch_dockerfile Dockerfile5
 docker build -f Dockerfile5 -t registry.ott-consult.de/oc/logstash:5 .
 docker push registry.ott-consult.de/oc/logstash:5
+
+patch_dockerfile Dockerfile6
 docker build -f Dockerfile6 -t registry.ott-consult.de/oc/logstash:6 .
 docker push registry.ott-consult.de/oc/logstash:6
 docker tag registry.ott-consult.de/oc/logstash:6 registry.ott-consult.de/oc/logstash:latest
 docker push registry.ott-consult.de/oc/logstash:6
 docker push registry.ott-consult.de/oc/logstash:latest
-
--- src/tmp/install/oc-logstash.sh
+++ src/tmp/install/oc-logstash.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source /tmp/install/functions.sh
 add_repos elasticsearch${LOGSTASH_VERSION:0:1}
 create_user_and_group
